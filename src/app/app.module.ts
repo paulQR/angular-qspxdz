35a5d97f2016e32edbb5a39bcfc8d08d4aadccd2
paulQR <paulqr10@gmail.com>
2019-07-29 11:39:58 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from'./components/protegida/protegida.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent,NavbarComponent, PreciosComponent, ProtegidaComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
